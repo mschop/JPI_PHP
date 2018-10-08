99ef02bb6c47632a60acaf7f49b7c68be44602bb
mschop <m.schophaus@adcada.de>
2018-10-08 09:14:22 +0200

    Use TreeQuery for ProductAttributeRepository

--- src/Db/Repository/ProductAttributeRepository.php
+++ src/Db/Repository/ProductAttributeRepository.php
@@ -10,6 +10,7 @@ use App\Db\Schema\tArtikelAttribut;
 use App\Db\Schema\tArtikelAttributSprache;
 use App\Db\Schema\tAttribut;
 use App\Db\Schema\tAttributSprache;
+use App\Db\TreeQueryFactory;
 use App\Mapping\MapperInterface;
 use App\Schema\Attribute;
 use App\Schema\AttributeTranslation;
@@ -23,17 +24,20 @@ class ProductAttributeRepository
     protected $mapper;
     protected $hydrator;
     protected $jsonHelper;
+    protected $treeQueryFactory;
 
     public function __construct(
         ConnectionProviderInterface $connectionProvider,
         MapperInterface $mapper,
         HydratorInterface $hydrator,
-        JsonHelperInterface $jsonHelper
+        JsonHelperInterface $jsonHelper,
+        TreeQueryFactory $treeQueryFactory
     ) {
         $this->connectionProvider = $connectionProvider;
         $this->mapper = $mapper;
         $this->hydrator = $hydrator;
         $this->jsonHelper = $jsonHelper;
+        $this->treeQueryFactory = $treeQueryFactory;
     }
 
     /**
@@ -43,35 +47,44 @@ class ProductAttributeRepository
      */
     public function getAll(int $tenantId, int $productId): array
     {
-        $conn = $this->connectionProvider->byTenantId($tenantId);
-        $sql = "
-            SELECT " . $this->hydrator->createSelect(tArtikelAttribut::class) . ",
-                (
-                    SELECT " . $this->hydrator->createSelect(tArtikelAttributSprache::class) . "
-                    FROM " . tArtikelAttributSprache::TABLE_NAME . " AS pattr_trans
-                    WHERE pattr_trans." . tArtikelAttributSprache::kArtikelAttribut . " = pattr." . tArtikelAttribut::kArtikelAttribut . "
-                    FOR JSON PATH
-                ) AS translations,
-                (
-                    SELECT " . $this->hydrator->createSelect(tAttribut::class) . ",
-                        (
-                            SELECT " . $this->hydrator->createSelect(tAttributSprache::class) . "
-                            FROM " . tAttributSprache::TABLE_NAME . " AS attr_trans
-                            WHERE attr_trans." . tAttributSprache::kAttribut . " = attr." . tAttribut::kAttribut . "
-                            FOR JSON PATH
-                        ) AS attributeTranslations
-                    FROM " . tAttribut::TABLE_NAME . " as attr
-                    WHERE attr." . tAttribut::kAttribut . " = pattr." . tArtikelAttribut::kAttribut . "
-                    FOR JSON PATH
-                ) AS attribute
-            FROM tArtikelAttribut AS pattr
-            WHERE kArtikel = :productId
-            FOR JSON PATH        
-        ";
+        $tArtikelAttribut_kArtikelAttribut = tArtikelAttribut::kArtikelAttribut;
+        $tArtikelAttribut_kAttribut = tArtikelAttribut::kAttribut;
+        $tArtikelAttributSprache_kArtikelAttribut = tArtikelAttributSprache::kArtikelAttribut;
+        $tAttribut_kAttribut = tAttribut::kAttribut;
+        $tAttributSprache_kAttribut = tAttributSprache::kAttribut;
 
-        $stmt = $conn->prepare($sql);
-        $stmt->execute(['productId' => $productId]);
-        $data = $this->jsonHelper->createResult($stmt);
+        $conn = $this->connectionProvider->byTenantId($tenantId);
+        $qb = $conn->createQueryBuilder();
+        $qb
+            ->from(tArtikelAttribut::TABLE_NAME, 'pattr')
+            ->select($this->hydrator->createSelect(tArtikelAttribut::class))
+            ->where(
+                $qb->expr()->eq(tArtikelAttribut::kArtikel, ':productId')
+            )
+            ->setParameter('productId', $productId)
+        ;
+
+        $treeQuery = $this->treeQueryFactory->build($qb);
+        $treeQuery->add(
+            tArtikelAttributSprache::class,
+            'translations',
+            'translations_sub',
+            "pattr.$tArtikelAttribut_kArtikelAttribut = translations_sub.$tArtikelAttributSprache_kArtikelAttribut"
+        );
+        $attrSub = $treeQuery->add(
+            tAttribut::class,
+            'attr',
+            'attr_sub',
+            "pattr.$tArtikelAttribut_kAttribut = attr_sub.$tAttribut_kAttribut"
+        );
+        $attrSub->add(
+            tAttributSprache::class,
+            'attrTrans',
+            'attrTrans_sub',
+            "attr_sub.$tAttribut_kAttribut = attrTrans_sub.$tAttributSprache_kAttribut"
+        );
+
+        $data = $treeQuery->execute();
 
         return map($data, function($pattr) {
             $tArtikelAttribut = $this->hydrator->toObject($pattr, tArtikelAttribut::class);
@@ -80,10 +93,10 @@ class ProductAttributeRepository
             $productAttribute->translations = map($pattr['translations'], function($pattrTrans) {
                 return $this->mapper->map($this->hydrator->toObject($pattrTrans, tArtikelAttributSprache::class), ProductAttributeTranslation::class);
             });
-            $attr = $this->hydrator->toObject($pattr['attribute'][0], tAttribut::class);
+            $attr = $this->hydrator->toObject($pattr['attr'][0], tAttribut::class);
             $productAttribute->attribute = new Attribute();
             $this->mapper->mapToObject($attr, $productAttribute->attribute);
-            $productAttribute->attribute->translations = map($pattr['attribute'][0]['attributeTranslations'], function($attrTranslation) {
+            $productAttribute->attribute->translations = map($pattr['attr'][0]['attrTrans'], function($attrTranslation) {
                 return $this->mapper->map(
                     $this->hydrator->toObject($attrTranslation, tAttributSprache::class),
                     AttributeTranslation::class
